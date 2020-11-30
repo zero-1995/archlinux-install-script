4cdf2f4a4dee038710b16757d0f5cc71daff0e7a
Shervin Valipour <shervin.valipour.1995@gmail.com>
2020-11-29 19:20:34 -0500

    fix nvidia

--- bin/post-installation.sh
+++ bin/post-installation.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 # Post Installation script
+yay(){
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+}
 
 basic(){
-    sudo pacman -Sy xorg xorg-xrandr ntfs-3g alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth pavucontrol bluez bluez-utils networkmanager network-manager-applet networkmanager-qt ttf-dejavu zip unzip unrar xarchiver 
+    sudo pacman -Sy --noconfirm xorg xorg-xrandr ntfs-3g alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth pavucontrol bluez bluez-utils networkmanager network-manager-applet networkmanager-qt ttf-dejavu zip unzip unrar xarchiver 
 }
 
 basicApps(){
-    sudo pacman -Sy flashplugin chromium firefox vlc nautilus gimp inkscape libreoffice
+    sudo pacman -Sy --noconfirm flashplugin firefox vlc nautilus gimp inkscape libreoffice
 }
 
 # TODO: Fix graphicsDriver function and nvidia setting
 graphicsDriver(){
-    sudo pacman -Sy nvidia nvidia-utils lib32-nvidia-utils xfce4 xfce4-goodies
+    sudo pacman -Sy --noconfirm nvidia nvidia-utils lib32-nvidia-utils xfce4 xfce4-goodies
 }
 
 programming(){
-	sudo pacman -Sy go python netbeans code atom leafpad android-file-transfer android-tools android-udev
+    sudo pacman -Sy --noconfirm code leafpad android-file-transfer android-tools android-udev
 }
 
 devOs(){
-        sudo pacman -Sy git docker
+    sudo pacman -Sy --noconfirm git docker
 }
 
 hack(){
-        sudo pacman -Sy proxychains-ng proxytunnel tor polipo
+    sudo pacman -Sy --noconfirm proxychains-ng proxytunnel tor polipo
 }
 
+#yay
 basic
-basicApps
+#basicApps
 graphicsDriver
-programming
-devOs
-hack
+#programming
+#devOs
+#hack
